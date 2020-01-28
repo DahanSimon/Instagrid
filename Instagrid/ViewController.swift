c2162e930c21e9b950813cdeac0939a74b15e997
DahanSimon <dahan.simon@hotmail.fr>
2020-01-28 19:59:54 +0100

    Improvement of the code architecture

--- Instagrid/ViewController.swift
+++ Instagrid/ViewController.swift
@@ -33,27 +33,82 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         if UIDevice.current.orientation.isPortrait {
             share()
         }
-        
     }
+    
     @objc func handleSwipeLeft() {
         if UIDevice.current.orientation.isLandscape {
             share()
         }
     }
-    @IBOutlet weak var photoLayoutView: photoLayoutView!
-    var selectedButton: UIButton?
     
+    func share() {
+        let image = viewToImage()
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        present(vc, animated: true)
+    }
+    
+    func viewToImage() -> UIImage{
+        let renderer = UIGraphicsImageRenderer(size: photoLayoutView.bounds.size)
+        let image = renderer.image { ctx in
+            photoLayoutView.drawHierarchy(in: photoLayoutView.bounds, afterScreenUpdates: true)
+        }
+        return image
+    }
+    
+    @IBOutlet weak var photoLayoutView: photoLayoutView!
+    
+    var selectedButton: UIButton?
     @IBAction func buttonTap(_ sender: UIButton) {
         selectedButton = sender
         selectingImage()
     }
     
+    func selectingImage() {
+        let selectImageController = UIImagePickerController()
+        selectImageController.delegate = self
+        
+        selectImageController.sourceType = UIImagePickerController.SourceType.photoLibrary
+        selectImageController.allowsEditing = false
+        
+        self.present(selectImageController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            selectedButton?.setImage(image, for: UIControl.State.normal)
+            selectedButton?.imageView?.contentMode = .scaleAspectFill
+            if let button = selectedButton {
+                grid.images[button.tag] = image
+            }
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
     
     @IBAction func layoutChange(_ sender: UIButton) {
         if let newLayout = Layout.init(rawValue: sender.tag) {
             setSelectedIcon(senderTag: sender.tag)
             grid.layout = newLayout
             photoLayoutView.updateFromGrid(grid: grid)
+        }
+    }
+    
+    func setSelectedIcon(senderTag: Int) {
+        if let selectedButton = self.view.viewWithTag(senderTag) as? UIButton {
+            for tag in 1...3 {
+                if tag == senderTag {
+                    selectedButton.setImage( #imageLiteral(resourceName: "Selected"), for: UIControl.State.normal)
+                }
+                else {
+                    if let button = self.view.viewWithTag(tag) as? UIButton {
+                        button.setImage(button.backgroundImage(for: UIControl.State.normal), for: UIControl.State.normal)
+                    }
+                }
+            }
+            
         }
     }
     
@@ -66,69 +121,6 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
                 shareLabel.text = "Swipe up to share"
             }
         }
-        
-        
     }
-    func selectingImage() {
-        let selectImageController = UIImagePickerController()
-        selectImageController.delegate = self
-        
-        selectImageController.sourceType = UIImagePickerController.SourceType.photoLibrary
-        selectImageController.allowsEditing = false
-        
-        self.present(selectImageController, animated: true, completion: nil)
-    }
-    
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
-            selectedButton?.setImage(image, for: UIControl.State.normal)
-            selectedButton?.imageView?.contentMode = .scaleAspectFill
-            if let button = selectedButton {
-                grid.images[button.tag] = image
-            }
-        }
-        else {
-//            if let button = self.view.viewWithTag(1) as? UIButton {
-//
-//            }
-        }
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    func setSelectedIcon(senderTag: Int) {
-        if let selectedButton = self.view.viewWithTag(senderTag) as? UIButton {
-            for tag in 1...3 {
-                if tag == senderTag {
-                    selectedButton.setImage(#imageLiteral(resourceName: "Selected.png"), for: UIControl.State.normal)
-                }
-                else {
-                    if let button = self.view.viewWithTag(tag) as? UIButton {
-                        button.setImage(button.backgroundImage(for: UIControl.State.normal), for: UIControl.State.normal)
-                    }
-                }
-            }
-            
-        }
-    }
-    
-    func viewToImage() -> UIImage{
-        let renderer = UIGraphicsImageRenderer(size: photoLayoutView.bounds.size)
-        let image = renderer.image { ctx in
-            photoLayoutView.drawHierarchy(in: photoLayoutView.bounds, afterScreenUpdates: true)
-        }
-        return image
-    }
-    
-    func share() {
-        let image = viewToImage()
-        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
-        present(vc, animated: true)
-    }
-    
 }
 
